8512f101120ac3456d658b59dfafe22e601c07fb
Xuesong Peng <penxuesong.cn@gmail.com>
2021-01-19 21:29:21 +0800

    Init commit

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by 彭雪松 on 2021/1/19.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
