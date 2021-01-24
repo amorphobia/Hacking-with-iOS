05e12c442eed4e1386f01c942723e70cc17af634
Xuesong Peng <pengxuesong.cn@gmail.com>
2021-01-24 16:19:04 +0800

    示例代码

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -7,10 +7,51 @@
 
 import SwiftUI
 
+//struct ContentView: View {
+////    @State private var tapCount = 0
+////    @State private var name = ""
+//    var body: some View {
+////        NavigationView {
+////            Form {
+////                Section {
+////                    Text("Hello, world!")
+////                        .padding()
+////                }
+////            }
+////            .navigationBarTitle("SwiftUI")
+////        }
+////        Button("Tap count: \(tapCount)"){
+////            self.tapCount += 1
+////        }
+////        Form {
+////            // 因此，当您在属性名称之前看到美元符号时，请记住，它会创建双向绑定：既读取属性值，又写入属性值。
+////            TextField("Enter your name", text: $name)
+////            Text("Your name is \(name)")
+////        }
+//        Form {
+////            ForEach(0 ..< 100) { number in
+////                Text("Row \(number)")
+////            }
+//            ForEach(0 ..< 100) {
+//                Text("Row \($0)")
+//            }
+//        }
+//    }
+//}
+
 struct ContentView: View {
+    let students = ["Harry", "Hermione", "Ron"]
+    @State private var selectedStudent = 0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Picker("Select your student", selection: $selectedStudent) {
+                ForEach(0 ..< students.count) {
+                    Text(self.students[$0])
+                }
+            }
+            Text("You chose: Student # \(students[selectedStudent])")
+        }
     }
 }
 
